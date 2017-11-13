bc23ed51253a2a0823352dec3b2fdb2bb574d559
kubik11 <kubyshkin2@gmail.com>
2017-11-13 23:20:00 +0300

    draw

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 (function (){
 $(document).ready(function(){
-	var circle = $('circle');
+	var circle = $('#circle');
+	var colorInput = $('#color');
+	var rangeInput = $('#slider');
 	$('#draw').css({
 		width: '100%',
 		height: '100%',
@@ -10,7 +12,31 @@ $(document).ready(function(){
 		var color = $(this).attr('data-color');
 		$(this).css('background', color);
 	});
+	// add event listener on color input to choose required color
+	colorInput.click(function(){
+		$('.wrap-to-choose').fadeIn();
+		//add event listener on color item list
+		$('.wrap-to-choose').click(function(e){
 
+			if(e.target.classList.contains('color-item')){	
+				var color;
+				color = e.target.dataset.color;
+				console.log(e);
+				colorInput.css('background-color', color).attr({"data-color": color});
+				circle.css('background-color', color)
+			}else{
+				$('.wrap-to-choose').fadeOut();
+			}
+		});
+	});
+	//add event listener on range input 
+	rangeInput.change(function(){
+		var val = $(this).val();
+		circle.css({
+			width: val+'px',
+			height: val+'px'
+		});
+	});
 });//ready function
 
 })()//I.I.F
